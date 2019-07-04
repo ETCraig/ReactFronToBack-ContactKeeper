fb96067771ef15d70f80325f71763d11bc9ee342
unknown <devethancraig@gmail.com>
2019-07-03 18:56:41 -0600

    Server Working

---
+++ Server.js
@@ -0,0 +1,9 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => res.json({msg: 'Welcome to the Contact Keeper API!'}));
+
+const PORT = process.env.PORT || 8000;
+
+app.listen(PORT, () => console.log(`Server is working on port: ${PORT}`));
