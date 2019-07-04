455f886313778639d429f4736a350fe27fb4e99b
unknown <devethancraig@gmail.com>
2019-07-04 15:30:20 -0600

    Secure Route && Middleware Added

--- routes/Users.js
+++ routes/Users.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const router = express.Router();
-const { check, validationResult } = require('express-validator');
 
-const user = require('../models/User');
+const bcrypt = require('bcryptjs');
+const { check, validationResult } = require('express-validator');
+const config = require('config');
+const jwt = require('jsonwebtoken');
+
+const User = require('../models/User');
 
 // @route   POST api/users
 // @desc    Register a user
@@ -13,12 +17,44 @@ router.post('/', [
     check('password', 'Please enter a password with 6 or more characters.').isLength({
         min: 6
     })
-], (req, res) => {
+], async (req, res) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
         return res.status(400).json({ errors: errors.array() });
     }
-    res.send('Passed');
+    const { name, email, password } = req.body;
+    try {
+        let user = await User.findOne({ email });
+        if (user) {
+            res.status(400).json({ msg: 'User already Exists.' });
+        }
+        user = new User({
+            name,
+            email,
+            password
+        });
+        const salt = await bcrypt.genSalt(10);
+
+        user.password = await bcrypt.hash(password, salt);
+
+        await user.save();
+
+        const payload = {
+            user: {
+                id: user.id
+            }
+        }
+
+        jwt.sign(payload, config.get('jwtSecret'), {
+            expiresIn: 36000
+        }, (err, token) => {
+            if (err) throw (err);
+            res.json({ token });
+        });
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server Error.');
+    }
 });
 
 module.exports = router;
