4b4fd7a06d819c1ae418583871ba14d824227aee
unknown <devethancraig@gmail.com>
2019-07-05 23:06:07 -0600

    Added Navbar, About, Home && Routing

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,25 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import './App.css';
+
+import About from './components/pages/About';
+import Home from './components/pages/Home';
+import NavBar from './components/layout/NavBar';
+
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 const App = () => {
   return (
-    <div className="App">
-      Contact Keaper
-    </div>
+    <Router>
+      <Fragment className="App">
+        <NavBar />
+        <div className="container">
+          <Switch>
+            <Route exact path='/' component={Home} />
+            <Route exact path='/about' component={About} />
+          </Switch>
+        </div>
+      </Fragment>
+    </Router>
   );
 }
 
