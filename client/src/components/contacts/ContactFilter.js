7b4eda8b8425f47467c2b9f31ba222fc2f34a504
unknown <devethancraig@gmail.com>
2019-07-10 01:45:08 -0600

    Finished Contact Filter

--- client/src/components/contacts/ContactFilter.js
+++ client/src/components/contacts/ContactFilter.js
@@ -1,14 +1,26 @@
-import React, { useContext, useRef } from 'react'
+import React, { useContext, useEffect, useRef } from 'react'
 
 import ContactContext from '../../context/contact/contactContext';
 
 const ContactFilter = () => {
     const contactContext = useContext(ContactContext);
 
+    const { filterContacts, clearFilter, filtered } = contactContext;
+
     const text = useRef('');
 
-    const onChange = e => {
+    useEffect(() => {
+        if (filtered === null) {
+            text.current.value = '';
+        }
+    })
 
+    const onChange = e => {
+        if (text.current.value !== '') {
+            filterContacts(e.target.value);
+        } else {
+            clearFilter();
+        }
     }
 
     return (
