cafa18ef6d53eed497dd2be283558ab27c406ddb
unknown <devethancraig@gmail.com>
2019-07-12 12:00:00 +0100

    Created Auth State - Reducer - Context - Types

--- client/src/App.js
+++ client/src/App.js
@@ -6,23 +6,26 @@ import Home from './components/pages/Home';
 import NavBar from './components/layout/NavBar';
 
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import AuthState from './context/auth/AuthState';
 import ContactState from './context/contact/ContactState';
 
 const App = () => {
   return (
-    <ContactState>
-      <Router>
-        <Fragment className="App">
-          <NavBar />
-          <div className="container">
-            <Switch>
-              <Route exact path='/' component={Home} />
-              <Route exact path='/about' component={About} />
-            </Switch>
-          </div>
-        </Fragment>
-      </Router>
-    </ContactState>
+    <AuthState>
+      <ContactState>
+        <Router>
+          <Fragment className="App">
+            <NavBar />
+            <div className="container">
+              <Switch>
+                <Route exact path='/' component={Home} />
+                <Route exact path='/about' component={About} />
+              </Switch>
+            </div>
+          </Fragment>
+        </Router>
+      </ContactState>
+    </AuthState>
   );
 }
 
