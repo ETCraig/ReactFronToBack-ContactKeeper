1da0d72ad28a035c782dab009d4e69fca54b0792
unknown <devethancraig@gmail.com>
2019-07-13 12:00:00 +0100

    Created Login  && Register Components w/ Nav

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,9 @@ import './App.css';
 
 import About from './components/pages/About';
 import Home from './components/pages/Home';
+import Login from './components/auth/Login';
 import NavBar from './components/layout/NavBar';
+import Register from './components/auth/Register';
 
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import AuthState from './context/auth/AuthState';
@@ -20,6 +22,8 @@ const App = () => {
               <Switch>
                 <Route exact path='/' component={Home} />
                 <Route exact path='/about' component={About} />
+                <Route exact path='/login' component={Login} />
+                <Route exact path='/register' component={Register} />
               </Switch>
             </div>
           </Fragment>
