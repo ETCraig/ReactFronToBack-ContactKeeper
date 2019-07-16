a1855a79c91f0b2c9cc02a254f0044bf4b545be3
unknown <devethancraig@gmail.com>
2019-07-16 17:55:51 -0600

    Login Working

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -1,6 +1,28 @@
-import React, { useState } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 
-const Login = () => {
+import AlertContext from '../../context/alerts/alertContext';
+import AuthContext from '../../context/auth/authContext';
+
+const Login = props => {
+    const alertContext = useContext(AlertContext);
+    const authContext = useContext(AuthContext);
+
+    const { setAlert } = alertContext;
+
+    const { login, error, clearErrors, isAuthenticated } = authContext;
+
+    useEffect(() => {
+        if (isAuthenticated) {
+            props.history.push('/');
+        }
+
+        if (error === 'Invalid Credentials') {
+            setAlert(error, 'danger');
+            clearErrors();
+        }
+        //eslint-disabled-next-line
+    }, [error, isAuthenticated, props.history]);
+
     const [user, setUser] = useState({
         email: '',
         password: '',
@@ -13,7 +35,14 @@ const Login = () => {
 
     const onSubmit = e => {
         e.preventDefault();
-        console.log('User Logged In');
+        if(email === '' || password === '') {
+            setAlert('Please fill in all fields', 'danger');
+        } else {
+            login({
+                email,
+                password
+            });
+        }
     }
 
     return (
--- client/src/context/auth/AuthState.js
+++ client/src/context/auth/AuthState.js
@@ -67,8 +67,27 @@ const AuthState = props => {
         }
     }
     //Login User
-    const login = () => {
+    const login = async formData => {
+        const config = {
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        }
+        try {
+            const res = await axios.post('/api/auth', formData, config);
 
+            dispatch({
+                type: LOGIN_SUCCESS,
+                payload: res.data
+            });
+            loadUser();
+        } catch (err) {
+            console.log(err.response.data.msg)
+            dispatch({
+                type: LOGIN_FAIL,
+                payload: err.response.data.msg
+            });
+        }
     }
     //Logout User
     const logout = () => {
