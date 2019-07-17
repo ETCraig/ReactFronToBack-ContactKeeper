53d2798d8abc23c0d85864504ef969dbea152201
unknown <devethancraig@gmail.com>
2019-07-16 18:41:05 -0600

    Now Retrieves Contacts from the DB && Presents a Spinner When Retrieving the Contacts

--- client/src/context/contact/ContactState.js
+++ client/src/context/contact/ContactState.js
@@ -2,6 +2,7 @@ import React, { useReducer } from 'react';
 import ContactContext from './contactContext';
 import contactReducer from './contactReducer';
 import {
+    GET_CONTACTS,
     ADD_CONTACT,
     DELETE_CONTACT,
     SET_CURRENT,
@@ -9,20 +10,37 @@ import {
     UPDATE_CONTACT,
     FILTER_CONTACTS,
     CLEAR_FILTER,
-    CONTACT_ERROR
+    CONTACT_ERROR,
+    CLEAR_CONTACTS
 } from '../Types';
 
 import axios from 'axios';
 
 const ContactState = props => {
     const initialState = {
-        contacts: [],
+        contacts: null,
         current: null,
         filtered: null,
         error: null
     }
     const [state, dispatch] = useReducer(contactReducer, initialState);
+    
+    //Get Users Contacts
+    const getContacts = async () => {
+        try {
+            const res = await axios.get('/api/contacts');
 
+            dispatch({ 
+                type: GET_CONTACTS, 
+                payload: res.data 
+            });
+        } catch (err) {
+            dispatch({
+                type: CONTACT_ERROR,
+                payload: err.response.msg
+            });
+        }
+    }
     //Add Contact
     const addContact = async contact => {
         const config = {
@@ -33,7 +51,10 @@ const ContactState = props => {
         try {
             const res = await axios.post('/api/contacts', contact, config);
 
-            dispatch({ type: ADD_CONTACT, payload: contact });
+            dispatch({ 
+                type: ADD_CONTACT, 
+                payload: contact 
+            });
         } catch (err) {
             dispatch({
                 type: CONTACT_ERROR,
@@ -72,6 +93,7 @@ const ContactState = props => {
                 current: state.current,
                 filtered: state.filtered,
                 error: state.error,
+                getContacts,
                 addContact,
                 deleteContact,
                 setCurrent,
