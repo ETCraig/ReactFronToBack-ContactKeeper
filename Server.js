8efdf097fb93ceccb9775628f860bc205dc01603
unknown <devethancraig@gmail.com>
2019-07-16 21:43:31 -0600

    Server PORT adjusted for Heroku

--- Server.js
+++ Server.js
@@ -21,6 +21,6 @@ if(process.env.NODE_ENV === 'production') {
     app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')));
 }
 
-const PORT = process.env.PORT || 8000;
-
-app.listen(PORT, () => console.log(`Server is working on port: ${PORT}`));
+app.listen(process.env.PORT || 3000, function(){
+    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+  });
